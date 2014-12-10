4b16b399fdb3c5eb1facf5fb2b55a0e5200d3dff
Sebastien Strban <strban.sebastien@gmail.com>
2014-12-10 19:01:17 +0000

    Urejanje

--- demo.js
+++ demo.js
@@ -147,7 +147,7 @@ function preberiEHRodBolnikaFirstName() {
 		    	if (res.length > 0) {
 			        temp = res[0].weight;
 			        temp2 = res[0].unit;
-			        $(".patient-weight").html("<button type=\"button\" class=\"btn btn-primary btn-xs\" onclick=\"kreirajEHRzaBolnika()\">Knof</button>");
+			        $(".patient-weight1").html("<button type=\"button\" class=\"btn btn-primary btn-xs\" onclick=\"kreirajEHRzaBolnika()\">Knof</button>");
 			        $(".patient-weight").html("<span class='obvestilo label label-success fade-in'>" + res[0].weight + " " + res[0].unit + "</span>");
 		    	} else {
 		    		$(".patient-weight").html("<span class='obvestilo label label-success fade-in'>" + "Ni podatkov" + "</span>");
