fbaa8ec366554c60bc33647c6d5c20c8f8060939
Sebastien Strban <strban.sebastien@gmail.com>
2014-12-10 12:24:29 +0000

    Urejanje

--- demo.js
+++ demo.js
@@ -103,7 +103,7 @@ function preberiEHRodBolnikaFirstName() {
 	    	success: function (data) {
 				var party = data.party;
 				$("#patient-name").html("<span class='obvestilo label label-success fade-in'>" + party.firstNames + " " + party.lastNames + ".</span>");
-				$("#patient-age").html("<span class='obvestilo label label-success fade-in'>" + (date().getFullYear()) + "</span>");
+				$("#patient-age").html("<span class='obvestilo label label-success fade-in'>" + (party.dateOfBirth) + "</span>");
 				//$("#patient-gender").html("<span class='obvestilo label label-success fade-in'>" + party.partyAdditionalInfo.telesnaVisina + "</span>");
 				$("#patient-dob").html("<span class='obvestilo label label-success fade-in'>" + party.dateOfBirth + "</span>");
 			},
