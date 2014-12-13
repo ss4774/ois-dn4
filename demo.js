34a7300b0471796ee5a6cd89ace2810fa27ef242
Sebastien Strban <strban.sebastien@gmail.com>
2014-12-13 13:37:09 +0000

    Urejanje

--- demo.js
+++ demo.js
@@ -106,10 +106,10 @@ function preberiEHRodBolnikaFirstName() {
 			headers: {"Ehr-Session": sessionId},
 	    	success: function (data) {
 				var party = data.party;
-				$("#patient-name").html("<span class='obvestilo label label-success fade-in'>" + party.firstNames + " " + party.lastNames + ".</span>");
-				$(".patient-age").html("<span class='obvestilo label label-success fade-in'>" + ( ((new Date).getFullYear() - (new Date(party.dateOfBirth)).getFullYear()) ) + "</span>");
+				$("#patient-name").html("<span class='C1'>" + party.firstNames + " " + party.lastNames + ".</span>");
+				$(".patient-age").html("<span class='C2'>" + ( ((new Date).getFullYear() - (new Date(party.dateOfBirth)).getFullYear()) ) + "</span>");
 				//$("#patient-gender").html("<span class='obvestilo label label-success fade-in'>" + party.partyAdditionalInfo.telesnaVisina + "</span>");
-				$("#patient-dob").html("<span class='obvestilo label label-success fade-in'>" + party.dateOfBirth + "</span>");
+				$("#patient-dob").html("<span class='C2'>" + party.dateOfBirth + "</span>");
 			},
 			error: function(err) {
 				$("#preberiSporocilo").html("<span class='obvestilo label label-danger fade-in'>Napaka '" + JSON.parse(err.responseText).userMessage + "'!");
