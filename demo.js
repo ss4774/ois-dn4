37be8e71724922c32138046e8d236900e1670d35
Sebastien Strban <strban.sebastien@gmail.com>
2014-12-21 13:43:30 +0000

    Urejanje

--- demo.js
+++ demo.js
@@ -317,14 +317,14 @@ function master_deatilWeight() {
 			            results += "<tr><td>" + rows[i].cas + "</td><td class='text-right'>" + rows[i].temperatura_vrednost + " " 	+ rows[i].temperatura_enota + "</td>";
 			        }
 			        results += "</table>";
-			        $("#rezultatMeritveVitalnihZnakov").append(results);
+			        $("#detail").html(results);
 		    	} else {
-		    		$("#preberiMeritveVitalnihZnakovSporocilo").html("<span class='obvestilo label label-warning fade-in'>Ni podatkov!</span>");
+		    		$("#detail").html("<span class='obvestilo label label-warning fade-in'>Ni podatkov!</span>");
 		    	}
 
 		    },
 		    error: function() {
-		    	$("#preberiMeritveVitalnihZnakovSporocilo").html("<span class='obvestilo label label-danger fade-in'>Napaka '" + JSON.parse(err.responseText).userMessage + "'!");
+		    	$("#detail").html("<span class='obvestilo label label-danger fade-in'>Napaka '" + JSON.parse(err.responseText).userMessage + "'!");
 				console.log(JSON.parse(err.responseText).userMessage);
 		    }
 		});
@@ -563,7 +563,7 @@ function preberiMeritveVitalnihZnakov() {
 					});					
 				} else if (tip == "telesna temperatura AQL") {
 					var AQL = 
-						/*"select " +
+						"select " +
     						"t/data[at0002]/events[at0003]/time/value as cas, " +
     						"t/data[at0002]/events[at0003]/data[at0001]/items[at0004]/value/magnitude as temperatura_vrednost, " +
     						"t/data[at0002]/events[at0003]/data[at0001]/items[at0004]/value/units as temperatura_enota " +
@@ -571,14 +571,6 @@ function preberiMeritveVitalnihZnakov() {
 						"contains OBSERVATION t[openEHR-EHR-OBSERVATION.body_temperature.v1] " +
 						"where t/data[at0002]/events[at0003]/data[at0001]/items[at0004]/value/magnitude<35 " +
 						"order by t/data[at0002]/events[at0003]/time/value desc " +
-						"limit 10";*/
-						"select " +
-    						"t/data[at0001]/events[at0002]/time/value as cas, " +
-    						"t/data[at0001]/events[at0002]/data[at0003]/items[at0004]/value as temperatura_vrednost, " +
-    						"t/data[at0001]/events[at0002]/data[at0003]/items[at0004]/value/units as temperatura_enota " +
-						"from EHR e[e/ehr_id/value='" + ehrId + "'] " +
-						"contains OBSERVATION t[openEHR-EHR-OBSERVATION.body_mass_index.v1] " +
-						"order by t/data[at0001]/events[at0002]/time/value desc " +
 						"limit 10";
 					$.ajax({
 					    url: baseUrl + "/query?" + $.param({"aql": AQL}),
@@ -940,4 +932,66 @@ function generirajBolnika() {
 	}
 	
 	
+}
+
+function graf3Djs(){
+	var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+	var x = d3.scale.ordinal()
+	    .rangeRoundBands([0, width], .1);
+	
+	var y = d3.scale.linear()
+	    .range([height, 0]);
+	
+	var xAxis = d3.svg.axis()
+	    .scale(x)
+	    .orient("bottom");
+	
+	var yAxis = d3.svg.axis()
+	    .scale(y)
+	    .orient("left")
+	    .ticks(10, "%");
+	
+	var svg = d3.select("body").append("svg")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+	  .append("g")
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	
+	d3.tsv("data.tsv", type, function(error, data) {
+	  x.domain(data.map(function(d) { return d.letter; }));
+	  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+	
+	  svg.append("g")
+	      .attr("class", "x axis")
+	      .attr("transform", "translate(0," + height + ")")
+	      .call(xAxis);
+	
+	  svg.append("g")
+	      .attr("class", "y axis")
+	      .call(yAxis)
+	    .append("text")
+	      .attr("transform", "rotate(-90)")
+	      .attr("y", 6)
+	      .attr("dy", ".71em")
+	      .style("text-anchor", "end")
+	      .text("Frequency");
+	
+	  svg.selectAll(".bar")
+	      .data(data)
+	    .enter().append("rect")
+	      .attr("class", "bar")
+	      .attr("x", function(d) { return x(d.letter); })
+	      .attr("width", x.rangeBand())
+	      .attr("y", function(d) { return y(d.frequency); })
+	      .attr("height", function(d) { return height - y(d.frequency); });
+	
+	});
+	
+	function type(d) {
+	  d.frequency = +d.frequency;
+	  return d;
+	}
 }
